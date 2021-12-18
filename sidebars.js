35eae798d2fe44aaa8dae2bc3c30e215d7cafd85
Frank Bakker <FrankBakkerNl@users.noreply.github.com>
2021-12-18 18:41:44 +0000

    add docs for Events and ServiceCallbacks

--- sidebars.js
+++ sidebars.js
@@ -22,6 +22,8 @@ module.exports = {
         'hass_model/hass_model_generated_entities',
         'hass_model/hass_model_generated_service',
         'hass_model/hass_model_working_with_entities',
+        'hass_model/hass_model_events',
+        'hass_model/hass_model_integration_servicecallback',
         'hass_model/hass_model_migration'
       ],
 
